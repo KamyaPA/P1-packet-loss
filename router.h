dfdf4651d59ecb80534d1215a3b9bd423233c7aa
Peter Hove <peteraksel@live.dk>
2021-11-29 11:04:31 +0100

    Added router

---
+++ router.h
@@ -0,0 +1,18 @@
+typedef struct RoutingTree{
+    int i;
+}RoutingTree;
+
+typedef struct Queue{
+    int length;
+    char *start;
+    char *read;
+    char *write;
+} Queue;
+
+typedef struct Router{
+    char *name;
+    struct Router *id; /*Pointer to it self*/
+    unsigned int speed;
+    RoutingTree *tree;
+    Queue queue;
+} Router;
