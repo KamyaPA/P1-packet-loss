b718448877fda61f66be9800d6530091309f1216
Peter Hove <peteraksel@live.dk>
2021-11-29 11:35:25 +0100

    Added packet-header

---
+++ packet.h
@@ -0,0 +1,10 @@
+typedef Host{
+    char hi;
+} Host;
+
+typedef struct PacketHeader{
+    unsigned int payload_length; /*How many bytes*/
+    char hop_limit;
+    Host *source_address;
+    Host *destination_address;
+} PacketHeader;
