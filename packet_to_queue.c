80e5178df871d9f1767ca5b7a0e3ef81ddca1095
Darth-Sand <92363336+Darth-Sand@users.noreply.github.com>
2021-12-15 13:53:14 +0100

    added minor changes, and made send function to read queue and retrieve a packet and then send it to another router. Added a space_left function to check if packet was sucessfully send/received

--- packet_to_queue.c
+++ packet_to_queue.c
@@ -5,6 +5,7 @@
 #include "router.h"
 #include "host.h"
 #include "packet.h"
+#include "create_function.h"
 PacketHeader create_packet(Host H1, Host H2);
 Router router_create(unsigned int speed_of_router, char *name_of_router, int length_of_queue, char *start_point, char *read_data, char *write_data);
 Host host_create(unsigned int speed_of_host, char *name_of_host, char *send, char *receive, Router R);
@@ -17,102 +18,130 @@ Host h;
 Host h2;
 Router r;
 
-h = host_create(1, "Host 1", r);
-h2 = host_create(1, "Host 2", r);
+h = host_create(1, "Host 1");
+h2 = host_create(1, "Host 2");
 r = router_create(1, "Router 1", 300);
 
-put_in_queue(r, create_packet(h,h2), 1);
-printf("%d", get_from_queue(r, 1));
 
 }
 
 
-PacketHeader create_packet(Host H1, Host H2){
+PacketHeader create_packet(Host H1, Host H2, void* where, int size_of_packet){
 
    PacketHeader P;
 
    P.hop_limit = 255; // number can be changed
    P.source_address = H1.address;
    P.destination_address = H2.address;
-   P.payload_length = sizeof(P.hop_limit) + sizeof(P.source_address) + sizeof(P.destination_address);
+   P.payload_length = (size_of_packet) - sizeof(PacketHeader);
 
    return(P);
 }
 
 Router router_create(unsigned int speed_of_router, char *name_of_router, int length_of_queue){
+    Router R;
+    R.type = ROUTER;
+    R.name = (char *) malloc (strlen(name_of_router));
+    strcpy(R.name, name_of_router);
+    R.id = &R;
+    R.speed = speed_of_router;
+    /*R.tree =*/ 
+    R.queue.length = length_of_queue;
+    R.queue.start = (char *) malloc (length_of_queue);
+    R.queue.read = R.queue.start;
+    R.queue.write = R.queue.start;
 
-Router R;
-R.name = *name_of_router;
-R.id = &R;
-R.speed = speed_of_router;
-//R.tree = 
-R.queue.length = length_of_queue;
-R.queue.start;
-R.queue.read;
-R.queue.write;
-
-return(R);
+    return(R);
 }
 
 // Speed, name, send, receive and router variabel of router struct will be input parameters
 
-Host host_create(unsigned int speed_of_host, char *name_of_host, Router R){
-
-Host H;
-H.name = *name_of_host;
-H.id = &H;
-H.speed = speed_of_host;
-H.address->id = &R;
-
-return(H);
+Host host_create(unsigned int speed_of_host, char *name_of_host){
+    Host H;
+    H.type = HOST;
+    H.name = (char *) malloc (strlen(name_of_host));
+    strcpy(H.name, name_of_host);
+    H.id = &H;
+    H.speed = speed_of_host;
+    H.Send = (char *) malloc (sizeof(PacketHeader) + 255);
+    H.Receive = (char *) malloc (sizeof(PacketHeader) + 255);
+    return(H);
 }
 
-void put_in_queue(Router R, PacketHeader P, int if_first){
+void put_in_queue(Router R, PacketHeader *P){
 
-   char *queue;
-   R.queue.start = *queue;
-   char *pack_size = &P;
+   char *pack_size = P;
 
-   queue = malloc((R.queue.length)*sizeof(char));
-   if(if_first == 1){ // if_first used to determine if packet is the first received in queue and whether write pointer has to be at beginning
-      R.queue.write = *queue;
-      for(int i = 0; i < ((P).payload_length); i++){
-      *(queue+i) = *(pack_size+i);
-      R.queue.write + i;
-   }
-   }else{
-      for(int i = 0; i < ((P).payload_length); i++){
-      *(queue+i) = *(pack_size+i);
-      R.queue.write + i;
-      }
-   }
-   free(queue);
+   for(int i = 0; i < ((P)->payload_length); i++){
+      *(R.queue.write++) = *(pack_size+i);
+      if((R.queue.write - R.queue.start) > R.queue.length) 
+         R.queue.write = R.queue.start;
+   }  
 }
 
-char get_from_queue(Router R, int if_first){
+char get_from_queue(Router R){
 
    PacketHeader P;
-   char *queue;
-   R.queue.start = *queue;
-   char *read = R.queue.read;
-   char *pack_size = &P;
+   char *pack_size = (char*)&P;
 
-   if(if_first == 1){
-      R.queue.read = *queue;
-      int payload =((PacketHeader*)read)->payload_length;
-      queue = malloc((payload)*sizeof(char));
-      for(int i = 0; i < payload; i++){
-         *(queue+i) = *(pack_size+i);
-         R.queue.read +i;
-      }
-   }else{
-      int payload =((PacketHeader*)read)->payload_length;
-      queue = malloc((payload)*sizeof(char));
-      for(int i = 0; i < payload; i++){
-         *(queue+i) = *(pack_size+i);
-         R.queue.read +i;
+   for(int i = 0; i < P.payload_length; i++){
+      *(R.queue.read++) = *(pack_size+i);
+      R.queue.read +i;
+   }
+   
+return pack_size;
+}
+
+int send(Router *from, Router *to){
+   char * PH = malloc(sizeof(PacketHeader));
+   char * Packet;
+   for(int i =0; i < sizeof(PacketHeader); i++){
+
+      *(PH+i) = *(from->queue.read++);
+      if(from->queue.read - from->queue.start > from->queue.length){
+         from->queue.read = from->queue.start;
       }
    }
-   free(queue);
-return pack_size;
+   (PacketHeader*)PH;
+   int length = ((PacketHeader *)PH)->payload_length;
+   Packet = malloc(sizeof(length));
+   for(int i =0; i < length; i++){
+      *(Packet+i) = *(from->queue.read++);
+      if(from->queue.read - from->queue.start > from->queue.length){
+         from->queue.read = from->queue.start;
+      }
+   }
+   if(length + sizeof(PacketHeader) < space_left(to)){
+      for(int i =0; i < sizeof(PacketHeader); i++){
+
+         *(to->queue.write++)= *(PH+i);
+         if(to->queue.write - to->queue.start > to->queue.length){
+            to->queue.write = to->queue.start;
+         }
+      }
+      for(int i =0; i < length; i++){
+         *(to->queue.write++) = *(Packet+i);
+         if(to->queue.write - to->queue.start > to->queue.length){
+            to->queue.write = to->queue.start;
+         }
+      }
+      return_val = 1;
+   }
+   else{
+      return_val = 0;      
+   }
+   free(PH);
+   free(Packet);
+   return return_val;
+}
+int space_left(Router *R){
+   if(R->queue.read > R->queue.write){
+      return R->queue.read - R->queue.write;
+   }
+   else if(R->queue.read < R->queue.write){
+      return R->queue.length - (R->queue.write - R->queue.read)
+   }
+   else{
+      return R->queue.length;
+   }
 }
